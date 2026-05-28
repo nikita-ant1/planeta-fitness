943959e7de1303422b07427f19627f8b271a6d29
Nikita Antonov <nikita45454@gmail.com>
2026-05-28 13:17:18 +0500

    style: обновляет дизайн главного экрана

--- src/components/Sections/Main.vue
+++ src/components/Sections/Main.vue
@@ -2,21 +2,34 @@
   <div class="main-block">
     <div class="wrapper">
       <div class="banner">
+        <div class="banner__title-wrap">
+          <h1 class="banner__title">Фитнес<br>на крыше</h1>
+        </div>
+        <div class="banner__period-wrap">
+          <p class="banner__period">июнь-сент</p>
+        </div>
+        <div class="banner__text-wrap" v-if="grid.md">
+          <p class="banner__text">
+            <span class="banner__text-l1">каждый день</span>
+            <span class="banner__text-l2">бесплатные занятия</span>
+            <span class="banner__text-l3">вместе</span>
+            <span class="banner__text-l4">с F-team</span>
+          </p>
+        </div>
         <picture>
           <source media="(max-width: 767.9px)" width="750" height="1080" srcset="@/assets/images/banner-sm.jpg" />
           <source media="(min-width: 768px)" width="2011" height="1131" srcset="@/assets/images/banner.jpg" />
           <img class="banner__img" src="@/assets/images/banner.jpg" width="2011" height="1131" alt="Фитнес на Крыше">
         </picture>
-        <div class="order-wrap">
-          <BaseButton class="order" @click="$order.show">Записаться</BaseButton>
-        </div>
       </div>
-      <h1 class="title" v-if="grid.lg">
-        Каждый день на крыше Планеты бесплатные занятия вместе с F-team
-      </h1>
-      <h1 class="title" v-else>
-        Каждый день бесплатные занятия вместе с F-team
-      </h1>
+      <div v-if="!grid.md" class="banner__text-sm-wrap">
+        <p class="banner__text-sm">
+          каждый день<br>бесплатные занятия<br>вместе с F-team<br>на крыше Планеты
+        </p>
+      </div>
+      <div class="order-wrap">
+        <BaseButton class="order" @click="$order.show">Записаться</BaseButton>
+      </div>
     </div>
   </div>
 </template>
@@ -29,64 +42,123 @@
 
 <style scoped lang="scss">
   .order-wrap {
-    position: absolute;
-    left: 0;
-    bottom: 0;
     width: 100%;
-    z-index: 10;
-    padding-bottom: 75px;
     text-align: center;
-
-    @include md {
-      padding-bottom: 30px;
-    }
   }
 
   .order {
     width: 100%;
-    max-width: 250px;
+    max-width: 365px;
   }
 
   .banner {
     position: relative;
     border-radius: 50px;
     overflow: hidden;
-    margin-bottom: 95px;
+    margin-bottom: 50px;
 
     @include md {
-      margin-bottom: 50px;
-    }
-
-    @include sm {
-      margin: 0 -16px 50px;
       border-radius: 40px;
-    }
-  }
-
-  .banner__img {
-    width: 100%;
-  }
-
-  .title {
-    font-weight: 400;
-    font-size: 34px;
-    line-height: 1.25;
-    letter-spacing: var(--lett-sp-sm);
-    color: var(--color-text-dark);
-    max-width: 604px;
-    margin: 0 auto;
-    text-align: center;
-
-    @include md {
-      font-size: 30px;
-      max-width: 580px;
+      margin-bottom: 30px;
     }
 
     @include sm {
-      font-size: 24px;
+      margin: 0 -16px 20px;
+      border-radius: 24px;
+    }
+
+    &__img {
+      width: 100%;
+    }
+
+    &__text-sm-wrap {
+      margin-bottom: 24px;
+    }
+
+    &__text-sm {
+      padding-left: 24px;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 1.2;
+      letter-spacing: 0.03em;
+    }
+
+    &__title-wrap {
+      position: absolute;
+      right: 17%;
+      bottom: 10.8%;
+
+      @include sm {
+        left: 12.6%;
+        bottom: 7.6%;
+      }
+    }
+
+    &__title {
+      font-weight: 600;
+      letter-spacing: 0.02em;
+      line-height: 1;
+      font-size: clamp(30px, 5.2vw, 103px);
+
+      @include sm {
+        font-size: clamp(32px, 11.7vw, 90px);
+      }
+    }
+
+    &__period-wrap {
+      position: absolute;
+      left: 30%;
+      bottom: 8.6%;
+
+      @include sm {
+        left: 13%;
+        bottom: 26%;
+      }
+    }
+
+    &__period {
+      font-weight: 500;
+      line-height: 1.2;
+      font-size: clamp(20px, 3.7vw, 75px);
+
+      @include sm {
+        line-height: 1;
+        font-weight: 600;
+        font-size: clamp(18px, 8vw, 62px);
+      }
+    }
+
+    &__text-wrap {
+      position: absolute;
+      left: 6%;
+      bottom: 9.5%;
+    }
+
+    &__text {
+      font-weight: 400;
       line-height: 1.1;
-      max-width: 275px;
-      letter-spacing: -0.01em;
+      letter-spacing: 0.03em;
+      font-size: clamp(16px, 2.5vw, 50px);
+      color: var(--color-white);
+
+      &-l1, &-l2, &-l3, &-l4 {
+        display: block;
+        width: fit-content;
+      }
+
+      &-l1 {
+        padding-left: 1.64em;
+      }
+
+      &-l2 {}
+
+      &-l3 {
+        padding-left: 0.58em;
+      }
+
+      &-l4 {
+        padding-left: 1.86em;
+      }
     }
   }
 </style>
